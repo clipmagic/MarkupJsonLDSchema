63fd415b07846542dc9a980a1c32e512df68b7c8
clipmagic <admin@clipmagic.com.au>
2025-09-29 13:17:10 +1000

    Upated

---
+++ schemas/jsonld.Custom.php
@@ -0,0 +1,16 @@
+<?php namespace ProcessWire;
+
+
+class JsonLDCustom extends WireData  {
+    
+    public function __construct() {
+    }
+    
+    public static function getSchema (array $data = null, Page $page = null) {    
+    // Use this class when you wish build your entire schema in the template
+    // Start the $options array in your template as $options = array('custom' => array( PUT YOUR SCHEMA STUFF HERE ));
+         
+         return;
+    }    
+} 
+?>
